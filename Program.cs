950537c4a86e44dffbb9f1837b693cb9d60b70e9
JNC <th3y@hotmail.com>
2024-02-23 00:46:50 -0500

    Changed behaviour + Remote play bridge added

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 using System.Threading;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Linq;
 
 namespace SteamLauncher
 {
@@ -13,78 +16,207 @@ namespace SteamLauncher
         [DllImport("user32.dll")]
         static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
-        public static class SteamLinkArgs
+        public static class ExeLinkArgs
         {
-            public static string epicUrl;
+            public static string url;
             public static string exeName;
             public static int timeout;
         }
-
         static void Main(string[] args)
         {
             var handle = GetConsoleWindow();
             ShowWindow(handle, 0); // 0 hide - 5 show
 
+            string appName = Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName);
             string launcherName = "EpicGamesLauncher";
-            if (args.Length < 2)
-            {
-                System.Console.WriteLine("ERROR: Needs launch URL and EXE Name");
-                return;
-            }
-            SteamLinkArgs.epicUrl = args[0];
-            SteamLinkArgs.exeName = args[1];
-
-            if (args.Length == 3)
-                SteamLinkArgs.timeout = int.Parse(args[2]);
 
             Ini iniFile = new Ini("config.ini");
 
-            string inival = iniFile.GetValue("RestartEpicGames").ToLower();
+            string filepath = iniFile.GetValue("GamePath").ToLower();
+            string timeoutstr = iniFile.GetValue("TimeOutSeconds").Trim();
 
-            if (inival != "false") {
-                foreach (var process in Process.GetProcessesByName(launcherName))
-                {
-                    process.Kill();
-                }
-            }
+            bool bUsingPathIni = false;
 
-            // Execute Epic Games Application
-            var ps = new ProcessStartInfo( SteamLinkArgs.epicUrl )
-            {
-                UseShellExecute = true,
-                Verb = "open"
-            };
-            Process.Start(ps);
-
-            System.Console.WriteLine($"Starting : { SteamLinkArgs.exeName }");
             
-            // TimeOut, because epic games launcher is too slow to run the game
-            inival = iniFile.GetValue("TimeOutSeconds");
-
-            if (inival.Length <= 1) inival = "60";
-            int waitTime = int.Parse(inival);
-
-            if (SteamLinkArgs.timeout >= 5) waitTime = SteamLinkArgs.timeout;
-
-            Thread.Sleep(waitTime * 1000);
-
-            var gameProcesses = Process.GetProcessesByName( SteamLinkArgs.exeName );
-
-            if (gameProcesses.Length != 1)
+            if (args.Length == 1)
             {
-                System.Console.WriteLine($"Could not find a single process with name: { SteamLinkArgs.exeName }");
+                ExeLinkArgs.exeName = args[0];
+            }
+            else if (args.Length == 2)
+            {
+                ExeLinkArgs.url = args[0];
+                ExeLinkArgs.exeName = args[1];
+            }
+            else if (filepath.Length != 0)
+            {
+                bUsingPathIni = true;
+                ExeLinkArgs.url = filepath;
+                ExeLinkArgs.exeName = Path.GetFileNameWithoutExtension(filepath);
+            }
+            else
+            {
+                Console.WriteLine("ERROR: Needs launch URL and EXE Name");
                 return;
             }
-            
-            System.Console.WriteLine($"Game started.");
-            gameProcesses[0].WaitForExit();
 
-            Thread.Sleep(2000);
+            var currentProcess = Process.GetCurrentProcess();
+            var pcss = Process.GetProcessesByName(currentProcess.ProcessName);
+            var anotherProcess = Process.GetProcesses().FirstOrDefault(p => p.ProcessName == currentProcess.ProcessName && p.Id != currentProcess.Id);
 
-            foreach (var process in Process.GetProcessesByName(launcherName))
-                process.Kill();
+            if (pcss.Length >= 3) return;
 
-            Environment.Exit(Environment.ExitCode);
+            // Slave Work
+            if (pcss.Length == 2 && anotherProcess != null && !bUsingPathIni)
+            {
+                Console.WriteLine($"Slave running and looking for my mastah");
+
+                while (true)
+                {
+                    var mainprocess = Process.GetProcesses().FirstOrDefault(p => p.ProcessName == currentProcess.ProcessName && p.Id != currentProcess.Id);
+                    if (mainprocess == null)
+                    {
+                        // Kill the game called from EpicGames
+                        foreach (var process in Process.GetProcessesByName(ExeLinkArgs.exeName))
+                            process.Kill();
+
+                        // Kill EpicGamesLauncher
+                        foreach (var process in Process.GetProcessesByName(launcherName))
+                            process.Kill();
+
+                        Environment.Exit(Environment.ExitCode);
+                        break;
+                    }
+
+                    Thread.Sleep(1000);
+                }
+
+                return;
+            }
+
+            if (args.Length == 3)
+                ExeLinkArgs.timeout = int.Parse(args[2]) * 1000;
+            else
+                ExeLinkArgs.timeout = (timeoutstr.Length != 0) ? int.Parse(timeoutstr) * 1000 : (70 * 1000);
+
+            Stopwatch detector = Stopwatch.StartNew();
+
+            if (bUsingPathIni)
+            {
+                // Execute Application
+                var ps = new ProcessStartInfo(ExeLinkArgs.url)
+                {
+                    UseShellExecute = true,
+                    WorkingDirectory = Path.GetDirectoryName(ExeLinkArgs.url),
+                    Verb = "open"
+                };
+                Process game = Process.Start(ps);
+
+                Console.WriteLine($"Starting from path: {ExeLinkArgs.exeName}");
+                Console.WriteLine($"Timeout: {ExeLinkArgs.timeout / 1000} sec");
+
+                bool gameIsRunning = false;
+
+                while (detector.ElapsedMilliseconds < ExeLinkArgs.timeout && !gameIsRunning)
+                {
+                    Process[] processes = Process.GetProcessesByName(ExeLinkArgs.exeName);
+                    foreach (Process proc in processes)
+                    {
+                        if (proc.Id == game.Id) // Check if the PID matches the process we started
+                        {
+                            Console.WriteLine($"Process {ExeLinkArgs.exeName} running, waiting until terminated.");
+
+                            detector.Stop();
+                            proc.WaitForExit();
+                            detector.Restart();
+                            gameIsRunning = true;
+                            break;
+                        }
+                    }
+
+                    Thread.Sleep(1000);
+                }
+
+                //if (detector.ElapsedMilliseconds >= SteamLinkArgs.timeout)
+                //{
+                //    Console.WriteLine($"Timeout ({SteamLinkArgs.timeout / 1000} seconds) reached. Exiting.");
+                //}
+                //else
+                //{
+                //    Console.WriteLine($"Process {SteamLinkArgs.exeName} exited, closing.");
+                //}
+
+                Thread.Sleep(500);
+                Environment.Exit(Environment.ExitCode);
+            }
+            else
+            {
+                // Kill Epic Games Launcher in case
+                string inival = iniFile.GetValue("RestartEpicGames").ToLower();
+
+                if (inival != "false")
+                {
+                    foreach (var process in Process.GetProcessesByName(launcherName))
+                    {
+                        process.Kill();
+                    }
+                }
+
+                // Execute Epic Games Launcher + Game parameter
+                var ps = new ProcessStartInfo(ExeLinkArgs.url)
+                {
+                    UseShellExecute = true,
+                    Verb = "open"
+                };
+                Process.Start(ps);
+
+                Console.WriteLine($"Starting: {ExeLinkArgs.exeName}");
+                Console.WriteLine($"Timeout: {ExeLinkArgs.timeout / 1000} sec");
+
+                if (pcss.Length == 1) // start slave
+                {
+                    string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                    Console.WriteLine($"Running slave");
+                    string strCmdPars = $"/C start /min \"\" /D \"{appDirectory}\" {appName} \"{ExeLinkArgs.exeName}\"";
+                    Process.Start("CMD.exe", strCmdPars);
+                }
+
+                while (detector.ElapsedMilliseconds < ExeLinkArgs.timeout)
+                {
+                    var gameProcess = Process.GetProcessesByName(ExeLinkArgs.exeName);
+                    if (gameProcess.Length > 0)
+                    {
+                        Console.WriteLine($"Process {ExeLinkArgs.exeName} running, waiting until terminated");
+
+                        detector.Stop();
+                        gameProcess[0].WaitForExit();
+                        detector.Restart();
+                        break;
+                    }
+                    else
+                    {
+                        Thread.Sleep(1000);
+                    }
+                }
+
+               
+                //if (detector.ElapsedMilliseconds >= SteamLinkArgs.timeout)
+                //{
+                //    Console.WriteLine($"Timeout ({SteamLinkArgs.timeout / 1000} seconds) reached. Exiting.");
+                //}
+                //else
+                //{
+                //    System.Console.WriteLine($"Process {SteamLinkArgs.exeName} exited, closing.");
+                //}
+
+                Thread.Sleep(500);
+
+                // Kill Epic Games Launcher
+                foreach (var process in Process.GetProcessesByName(launcherName))
+                    process.Kill();
+
+                Environment.Exit(Environment.ExitCode);
+
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,21 +1,39 @@
-# SteamLink
-Epic Games linked cross SteamGames on BigPicture
+# SteamLinkTool
 This is a simple application to execute EpicGames games (lol) through steam (Using BigPicture/SteamLink)
 I made this because i wanted to play some games from epic games using steamlink and bigpicture.
 
 1- Add a new shortcut to steam
 
-2- Select steamlink as exe
+2- Select SteamLinkTool as EXE
 
-3- Get the shortcut url from EpicGames and set it as paremeter. example:
+3- Get the shortcut url from EpicGames and set it as parameter on steam app properties. example:
     com.epicgames.launcher://apps/0584d2013f0149a791e7b9bad0eec102%3A6e563a2c0f5f46e3b4e88b5f4ed50cca%3A9d2d0eb64d5c44529cece33fe2a46482?action=launch&silent=true
     
-4- Parameters are like:
-    (epic games url) (Executable game name) (Timeout in seconds:Optional)
+4- Parameters are like: [epic games url] [Executable game name] [Timeout in seconds:Optional]
     
 5- Final Parameter
   com.epicgames.launcher://apps/0584d2013f0149a791e7b9bad0eec102%3A6e563a2c0f5f46e3b4e88b5f4ed50cca%3A9d2d0eb64d5c44529cece33fe2a46482?action=launch&silent=true GTA5
 
-
-This app is doing a timeout (65 seconds default). SteamLink will launch Epic Games Launcher and execute the game..., this will take time (Timeout Secs) 
+This app is doing a timeout (70 seconds default). SteamLink will launch Epic Games Launcher and execute the game..., this will take time (Timeout Secs) 
 I made this app a few months ago. This is just a backup. SteamLink/EpicGames probably works good without this app.
+
+
+### There is also a new feature, this could help users using RemotePlay, like, playing Need For Speed Carbon, but letting your friend to play the game through RemotePlay.
+
+- Install DEAD (steam://install/952700) or any other game that lets you use the Steam API input or Xbox Controller, and have remoteplay activated.
+- Replace DEAD.exe for SteamLinkTool executable
+- Configure Config.ini and set as GamePath= value the executable game you would like to play, example C:\Games\MycoolGame.exe
+- Start DEAD from steam, and invite your friend to the game
+
+
+### How it works:
+
+- Steam runs SteamlinkTool. If you run an app through SteamLink, SteamLink will activate the controller compatibility.
+- SteamLinkTool open EpicGames launcher with the parameters (Game URL)
+- EpicGamesLauncher launch 'FallGuys', steam hooks the controller to the game through SteamLinkTool.
+- SteamLinkTool open twice but the second app is a slave that will check if the first steamlinktool is running.
+- You open SteamLink menu and choose to End the game. Steam close first steamlinktool, slave steamlinktool detect their master is gone, and close the game and EpicGamesLauncher.
+- You are no able to play another game.
+
+
+
